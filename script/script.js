aa4a0c7a85d5b84018cec563ef5a58b84b7979b8
TheNikeSpike <108290989+TheNikeSpike@users.noreply.github.com>
2024-07-02 20:35:25 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -38,11 +38,11 @@ let flick = function() {
 
 }
 
-var countDownDate = new Date('July 13, 2024 00:00:00').getTime();
+var countDownDate = new Date('July 13, 2024 00:00:00');
 
 var x = setInterval(function () {
   var now = new Date().getTime();
-  var distance = countDownDate - now;
+  var distance = countDownDate.getTime() - now;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
